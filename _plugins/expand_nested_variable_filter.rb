5cec09a671f3b581339d0f416741045a6338eb2f
Amy Rose <alr6764@rit.edu>
2022-05-24 13:33:03 -0400

    rework templates, make contact info configurable

---
+++ _plugins/expand_nested_variable_filter.rb
@@ -0,0 +1,11 @@
+# credit: http://acegik.net/blog/ruby/jekyll/plugins/howto-nest-liquid-template-variables-inside-yaml-front-matter-block.html
+
+module Jekyll
+  module ExpandNestedVariableFilter
+    def flatify(input)
+      Liquid::Template.parse(input).render(@context)
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::ExpandNestedVariableFilter)
